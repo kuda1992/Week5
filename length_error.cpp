fb67ad5c06bf0a64a6d1fbe00b41cfaa78e04a42
Kuda <kuda.nyamainashe@ext.soprasteria.com>
2019-09-04 08:28:14 +0100

    Initial commit

---
+++ length_error.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kuda on 2019-08-29.
+//
+
+#include "length_error.h"
---
+++ length_error.h
@@ -0,0 +1,14 @@
+//
+// Created by Kuda on 2019-08-29.
+//
+
+#ifndef WEEK5_LENGTH_ERROR_H
+#define WEEK5_LENGTH_ERROR_H
+
+
+class length_error {
+
+};
+
+
+#endif //WEEK5_LENGTH_ERROR_H
